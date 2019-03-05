aedc21dfaa2da6161a1e2bae1ba3ee3ad5d1780b
Sunil.Jacob <sunil1405@gmail.com>
2019-03-05 14:47:18 +0530

    Added sonar

--- src/main/java/com/example/workshop/demo/hello/HelloController.java
+++ src/main/java/com/example/workshop/demo/hello/HelloController.java
@@ -1,23 +1,14 @@
 package com.example.workshop.demo.hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-//    private FeatureManager featureManager;
-//
-//    public HelloController(FeatureManager featureManager) {
-//        this.featureManager = featureManager;
-//    }
-
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String greetingPhilips() {
-
-//        if(FeatureToggles.FEATURE_ONE.isActive()) {
-//            return "Hello Philips";
-//        }
         return "Hello Philips";
     }
 }
